ba7beaf2e9effa7dba5f459d02fe31e078cfab2b
Liza Lopez <liza.b.lopez@gmail.com>
2015-10-13 14:06:02 -0600

    Added solutions for remaining collections(every, some)

--- src/underbar.js
+++ src/underbar.js
@@ -220,14 +220,40 @@
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
-    // TIP: Try re-using reduce() here.
-   
+    // TIP: Try re-using reduce() here
+      
+    return _.reduce(collection, function(remainsTrue, item) {
+      if (iterator == null || iterator == undefined) {
+        return true ? _.identity(item) === true : false;
+      }
+      else {
+        if (!remainsTrue) {
+          return false;
+        } else  {
+          return (iterator(item)) ? true : false;
+        }
+      }
+    }, true); 
   };
-
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+
+    //if we use !_.every and test if elements match a truth test, it would return
+    //false when all elements are true and none are false, and vice versa
+    //if we test if elements don't match a true test (returning true for falsy
+    //items and false for truthy items), every() would return return false as soon
+    //as it encounters a truthy element, and since we're returning !every(),
+    //the function would return as true
+
+    return !_.every(collection, function(value) {
+      if (iterator == null || iterator == undefined) {
+        iterator = _.identity;
+      }
+      return !(iterator(value)) ? true : false;
+    });
+
   };
 
 
@@ -250,6 +276,7 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    
   };
 
   // Like extend, but doesn't ever overwrite a key that already
